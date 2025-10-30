041bcd8ef4fbeef43a90e93c89851975af39e40e
snow_allay <165352905+snowallay@users.noreply.github.com>
2025-10-30 17:39:45 +0900

    Update proxy1.js

--- proxy1.js
+++ proxy1.js
@@ -1,9 +1,2 @@
-function butotnClick(){
-  test.innerText = textbox.value + 'に飛ぶ';
-}
-
-let textbox = document.getElementById('textbox');
-let test = document.getElementById('test');
-
-let textbox_button = document.getElementById('textbox_button');
-textbox_button.addEventListener('click', butotnClick);
+var substr = location.pathname
+console.log(substr)
